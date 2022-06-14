8e644dd6c086e0b536276554f40efcf67a9a733e
Johnny Hauser <johnnyhauser@gmail.com>
2022-06-14 15:49:15 -0500

    rollup and babel setup for dist build

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import linaria from '@linaria/rollup'
+import postcss from 'rollup-plugin-postcss'
+import resolve from '@rollup/plugin-node-resolve'
+import babel_config from './.babelrc.json'
+import pkg from './package.json'
+
+const plugins = [
+	resolve({
+		browser: true
+	}),
+	commonjs(),
+	linaria({
+		preprocessor: 'none',
+		sourceMap: false
+	}),
+	postcss({
+		extract: false,
+		plugins: []
+	}),
+	babel({
+		babelHelpers: 'runtime',
+		sourceMaps: false,
+		compact: false,
+		...babel_config
+	})
+]
+
+export default {
+	external: [
+		/@babel\/runtime/,
+		...Object.keys(pkg.peerDependencies)
+	],
+	input: './src/index.js',
+	output: [
+		{
+			format: 'esm',
+			file: pkg.module,
+			sourcemap: false
+		},
+		{
+			format: 'cjs',
+			file: pkg.main,
+			sourcemap: false
+		}
+	],
+	plugins
+}
