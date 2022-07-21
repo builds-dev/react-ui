87865aae82b5f9160911cd753a3df37b567ecc26
Johnny Hauser <johnnyhauser@gmail.com>
2022-07-21 11:16:51 -0500

    forward refs for Box, Column, Row

--- src/Column.jsx
+++ src/Column.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { column, compute_style_for_layout_y_parent, compute_style_for_layout_y_child } from './layout.js'
 import { Layout_box } from './Layout_box.jsx'
 
-export const Column = props =>
+export const Column = forwardRef((props, ref) =>
 	<Layout_box
+		ref={ref}
 		layout_class_name={column}
 		compute_style_as_layout_parent={compute_style_for_layout_y_parent}
 		compute_style_for_layout_child={compute_style_for_layout_y_child}
 		{...props}
 	/>
+)
--- src/Row.jsx
+++ src/Row.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { row, compute_style_for_layout_x_parent, compute_style_for_layout_x_child } from './layout.js'
 import { Layout_box } from './Layout_box.jsx'
 
-export const Row = props =>
+export const Row = forwardRef((props, ref) =>
 	<Layout_box
+		ref={ref}
 		layout_class_name={row}
 		compute_style_as_layout_parent={compute_style_for_layout_x_parent}
 		compute_style_for_layout_child={compute_style_for_layout_x_child}
 		{...props}
 	/>
+)
