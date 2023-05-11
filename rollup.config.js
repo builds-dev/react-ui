6c1873c25e5dd643fc3499b37393c7f6be26cdf1
Johnny Hauser <johnnyhauser@gmail.com>
2023-05-10 23:59:45 -0500

    update rollup config to work on node 19

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
-import linaria from '@linaria/rollup'
+import _linaria from '@linaria/rollup'
 import postcss from 'rollup-plugin-postcss'
 import resolve from '@rollup/plugin-node-resolve'
 import babel_config from './.babelrc.json' assert { type: 'json' }
-import pkg from './package.json'
+import pkg from './package.json' assert { type: 'json' }
+const linaria = _linaria.default
 
 const plugins = [
 	resolve({
